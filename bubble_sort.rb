fd8737d07144252a26e734eeceb40153d2e5b576
CyprianS <szwajka.cyprian@gmail.com>
2017-01-16 15:14:18 +0100

    done

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,32 @@
 def bubble_sort(numbers)
-	counter = 0
-	while counter < numbers.length
+	change = true
+	while change
+		change = false
 		0.upto(numbers.length - 2) do |n|
 			if numbers[n] > numbers[n+1]
 				numbers[n], numbers[n+1] = numbers[n+1], numbers[n]
-				counter = 0
-			else 
-				counter += 1
+				change = true
 			end
 		end	
 	end
 	numbers
 end
 
+def bubble_sort_by(sort)
+	change = true
+	while change
+		change = false
+		0.upto(sort.length - 2) do |n|
+			if yield(sort[n],sort[n+1]) == 1
+				sort[n], sort[n+1] = sort[n+1], sort[n]
+				change = true
+			end
+		end
+	end
+	sort
+end
+
 start_time = Time.now
 p bubble_sort([4,3,78,2,0,2])
+p bubble_sort_by(["hi","hello","hey"]) {|left, right| right <=> left }
 p "Time elapsed: #{(Time.now - start_time)*1000} milliseconds"
--- test.rb
+++ test.rb
@@ -1,36 +1,31 @@
-=begin
-def bubble_sort(numbers)
-	counter = 0
-	unless counter >= numbers.length
-		0.upto(numbers.length - 1) do |n|
-			if numbers[n] > numbers[n+1]
-				numbers[n], numbers[n+1] = numbers[n+1], numbers[n]
-				counter = 0
-			else 
-				counter += 1
+def bubble_sort array
+	swap = true
+	while swap
+		swap = false
+		for i in 0..array.length-2
+			if array[i] > array[i+1]
+				array[i], array[i+1] = array[i+1], array[i]
+				swap = true
 			end
-		end	
-	end
-	return numbers
-end
-
-puts bubble_sort([2,5,1,2,3])
-=end	
-
-def bubble_sort(numbers)
-	puts numbers
-	puts numbers.length
-	counter = 0
-
-	0.upto(numbers.length - 2) do |n|
-		if numbers[n] > numbers[n+1]
-			numbers[n], numbers[n+1] = numbers[n+1], numbers[n]
-			counter = 0
-		else 
-			counter += 1
 		end
-		puts counter
 	end
+	return array
 end
 
-bubble_sort([1,2,52,31,5341])
+def bubble_sort_by array
+	swap = true
+	while swap
+		swap = false
+		for i in 0..array.length-2
+			y = yield(array[i],array[i+1])
+			if y == 1
+				array[i], array[i+1] = array[i+1], array[i]
+				swap = true
+			end
+		end
+	end
+	return array
+end
+array = bubble_sort_by(["hi","hello","hey"]) {|left,right| right <=> left}
+
+puts array
